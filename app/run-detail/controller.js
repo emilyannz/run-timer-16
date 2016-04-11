c41d74c9a7d9380d782cd14f04d2e67a86bd6069
Emily Znamierowski <emilyannz@gmail.com>
2016-04-11 13:49:01 -0500

    Fixed some linter errors

--- app/run-detail/controller.js
+++ app/run-detail/controller.js
@@ -3,34 +3,33 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   saveCurrentRun() {
 
-   const runInfo = {
-     runTime: this.runTime,
-     runDate: this.runDate,
-     raceNote: this.raceNote,
-   };
+    const runInfo = {
+      runTime: this.runTime,
+      runDate: this.runDate,
+      raceNote: this.raceNote,
+    };
 
-   const model = this.model;
-   const url = `http://tiny-tn.herokuapp.com/collections/runs-jw/${model._id}`;
+    const model = this.model;
+    const url = `http://tiny-tn.herokuapp.com/collections/runs-ez/${model._id}`;
 
-   const fetchOptions = {
-     method: `PUT`,
-     headers: {
-       Accept: `application/json`,
-       'Content-type': `application/json`,
-     },
-     body: JSON.stringify(runInfo),
-   };
+    const fetchOptions = {
+      method: `PUT`,
+      headers: {
+        Accept: `application/json`,
+        'Content-type': `application/json`,
+      },
+      body: JSON.stringify(runInfo),
+    };
 
-   fetch(url, fetchOptions)
-     .then((response) => response.json())
-     .then((run) => {
-       this.addNewUpdatedRun(run);
-       // this.transitionToRoute(`index`);
-     });
- },
+    fetch(url, fetchOptions)
+      .then((response) => response.json())
+      .then((run) => {
+        this.addNewUpdatedRun(run);
+        // this.transitionToRoute(`index`);
+      });
+  },
 
- addNewUpdatedRun(run) {
-   this.set(`model`, [run, ...this.model]);
- },
-});
+  addNewUpdatedRun(run) {
+    this.set(`model`, [run, ...this.model]);
+  },
 });
