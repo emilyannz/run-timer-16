a256210a0015566f5fb6aae54743f95b71450c86
Emily Znamierowski <emilyannz@gmail.com>
2016-04-11 13:49:36 -0500

    Fixed more linter errors

--- app/new/controller.js
+++ app/new/controller.js
@@ -2,39 +2,39 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   saveNewRun() {
-  const runInfo = {
-    runTime: this.runTime,
-    runDate: this.runDate,
-    raceNote: this.raceNote,
-  };
+    const runInfo = {
+      runTime: this.runTime,
+      runDate: this.runDate,
+      raceNote: this.raceNote,
+    };
 
-  const url = `http://tiny-tn.herokuapp.com/collections/runs-ez`;
+    const url = `http://tiny-tn.herokuapp.com/collections/runs-ez`;
 
-  const fetchOptions = {
-    method: `POST`,
-    headers: {
-      Accept: `application/json`,
-      'Content-type': `application/json`,
-    },
-    body: JSON.stringify(runInfo),
-  };
+    const fetchOptions = {
+      method: `POST`,
+      headers: {
+        Accept: `application/json`,
+        'Content-type': `application/json`,
+      },
+      body: JSON.stringify(runInfo),
+    };
 
-  fetch(url, fetchOptions)
-    .then((response) => response.json())
-    .then((run) => {
-      this.addNewRun(run);
-      this.clearForm();
-      this.transitionToRoute(`index`);
-    });
-},
+    fetch(url, fetchOptions)
+      .then((response) => response.json())
+      .then((run) => {
+        this.addNewRun(run);
+        this.clearForm();
+        this.transitionToRoute(`index`);
+      });
+  },
 
-clearForm() {
-  this.set(`runTime`, ``);
-  this.set(`runDate`, ``);
-  this.set(`raceNote`, ``);
-},
+  clearForm() {
+    this.set(`runTime`, ``);
+    this.set(`runDate`, ``);
+    this.set(`raceNote`, ``);
+  },
 
-addNewRun(run) {
-  this.set(`model`, [run, ...this.model]);
-},
+  addNewRun(run) {
+    this.set(`model`, [run, ...this.model]);
+  },
 });
